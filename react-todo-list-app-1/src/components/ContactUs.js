115911d8879fb6e21eefd40822f7fd6c398dafbd
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-21 18:09:33 +0000

    update react-todo-list-app-1, contactus

--- react-todo-list-app-1/src/components/ContactUs.js
+++ react-todo-list-app-1/src/components/ContactUs.js
@@ -1,8 +1,24 @@
 function ContactUs() {
     return (
       <>
-        <h4>ContactUs page/component</h4>
-      </>
+        <p></p>
+        <p>Contact Us</p>
+        <p>{"msg"}</p>
+        <div className="output"></div>
+        <p></p>
+        <div>
+          <form>
+            <label>* Name: </label><input name="txtname" type="text" maxLength={20}></input><br></br>
+            <label>* Email: </label><input name="txtemail" type="text"  maxLength={30}></input><br></br>
+            <label> Phone: </label><input name="txtphone" type="text"  maxLength={15}></input><br></br>
+            <label>* Comment: </label><br></br>
+            <textarea name="txtcomment" rows={5} cols={30}></textarea><p></p>
+            <button>Submit</button>
+            {"   "}
+            <button>reset</button>
+        </form>
+      </div>
+    </>
     );
   }
   
