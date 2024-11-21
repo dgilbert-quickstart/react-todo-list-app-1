e6729136f9d125767a44bf6a88c184a45f4e0937
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-21 23:03:48 +0000

    update react-todo-list-app-1, todolist

--- react-todo-list-app-1/src/components/ContactUs.js
+++ react-todo-list-app-1/src/components/ContactUs.js
@@ -8,14 +8,14 @@ function ContactUs() {
         <p></p>
         <div>
           <form>
-            <label>* First Name: </label><input name="txtfirstname" type="text" maxLength={20}></input><br></br>
-            <label>* Last Name: </label><input name="txtlastname" type="text" maxLength={20}></input><br></br>
-            <label>* Email: </label><input name="txtemail" type="text"  maxLength={30}></input><br></br>
+            <label>* First Name: </label><input name="txtfirstname" type="text" maxLength={20} placeholder="* firstname required" /><br></br>
+            <label>* Last Name: </label><input name="txtlastname" type="text" maxLength={20}  placeholder="* lastname required"/><br></br>
+            <label>* Email: </label><input name="txtemail" type="text"  maxLength={30}  placeholder="* email required"/><br></br>
             <label>* Comment: </label><br></br>
-            <textarea name="txtcomment" rows={5} cols={30}></textarea><p></p>
+            <textarea name="txtcomment" rows={5} cols={30}  placeholder="* comment required"/><p></p>
             <button>Submit</button>
             {"   "}
-            <button>reset</button>
+            <a href="#/">reset</a>
         </form>
       </div>
     </>
--- react-todo-list-app-1/src/components/Home.js
+++ react-todo-list-app-1/src/components/Home.js
@@ -3,7 +3,7 @@ import TodoList from "./TodoList";
 function Home() {
     return (
       <>
-        <TodoList title="React TodoList App 1.0.9"/>
+        <TodoList title="React TodoList App 1.1.0"/>
       </>
     );
   }
