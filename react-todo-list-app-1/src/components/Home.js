7fbd39b71d7c9feb48e5a36596e9e30a214fc424
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-21 20:57:30 +0000

    update react-todo-list-app-1, todolist

--- react-todo-list-app-1/src/components/Home.js
+++ react-todo-list-app-1/src/components/Home.js
@@ -3,12 +3,11 @@ import TodoList from "./TodoList";
 function Home() {
     return (
       <>
-        <TodoList title="React TodoList App 1.0.6"/>
+        <TodoList title="React TodoList App 1.0.7"/>
       </>
     );
   }
   
   export default Home;
 
-  
   
--- react-todo-list-app-1/src/components/TodoList.js
+++ react-todo-list-app-1/src/components/TodoList.js
@@ -2,6 +2,30 @@ function TodoList(props) {
     return (
       <>
         <h4>{props.title}</h4>
+        <div>
+          {"item [   ] add reset"}
+        </div>
+        <div>
+          <p>{" msg "}</p>
+        </div>
+        <div>
+          {" all | completed | in-completd"}
+        </div>
+        <p></p>
+        <div>
+          <p>
+            {"[x] item one [x]"}
+          </p>
+          <p>
+            {"[] item two [x]"}
+          </p>
+          <p>
+            {"[x] item three [x]"}
+          </p>
+          <p>
+            {"[] item four [x]"}
+          </p>
+        </div>
       </>
     );
   }
@@ -9,4 +33,3 @@ function TodoList(props) {
   export default TodoList;
   
 
-  
