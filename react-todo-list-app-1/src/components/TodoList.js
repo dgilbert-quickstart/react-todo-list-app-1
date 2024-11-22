a43a69e8a34dc27c96a4774cc3f88f720cc6e909
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-22 18:36:18 +0000

    update react todolist app 1, contactus

--- react-todo-list-app-1/src/components/TodoList.js
+++ react-todo-list-app-1/src/components/TodoList.js
@@ -3,11 +3,40 @@ import { useEffect, useState } from "react";
 function TodoList(props) {
 
   const [msg, setMsg] = useState("");
+  const [txtnewitem, setNewItem] = useState("");
 
     useEffect((e)=>{
 
 
     },[]) //1. onpage load [], 2. page unload, 3. on state update [msg], 4. un page /refresh
+
+    const handleAddNewItem = (e)=>{
+      try {
+
+        let _msg = "# handleAddNewItem"
+
+        if(txtnewitem == null | txtnewitem.trim().length == 0){
+
+          _msg = "# please enter a valid item"
+          setMsg(_msg)
+          return false; //stop button click 
+        }
+        
+        _msg = "# new item added"
+        setMsg(_msg)
+
+        console.log(_msg)
+        console.log(txtnewitem)
+
+      } catch (error) {
+        
+        let _msg = "# handleAddNewItem::error"
+        setMsg(_msg)
+
+        console.log(_msg)
+        console.log(error)
+      }
+    }
 
     return (
       <>
@@ -15,11 +44,11 @@ function TodoList(props) {
          <h4>{props.title}</h4>
          <div>
           <span>item: </span>
-          <input type="text" name="txtitemname" maxLength={25} placeholder="* add new item"></input>
+          <input type="text" value={txtnewitem} onChange={(e)=>setNewItem(e.target.value)} name="txtitemname" maxLength={25} placeholder="* add new item"></input>
           {" "}
-          <button>Add</button>
+          <button onClick={handleAddNewItem}>Add</button>
           {" "}
-          <a href="#/">reset</a>
+          <a href="#/" onClick={(e)=>{setNewItem(""); setMsg("")}}>reset</a>
         </div>
         <div>
           <p>{msg}</p>
